b2f415ae2af1c4d549e699e7b00d7f2a4cd19b3d
Teo Espero <tespero@wgu.edu>
2018-08-30 22:52:21 +0000

    initial commit

---
+++ npm-bax/demo-app/app.js
@@ -0,0 +1,16 @@
+// Teo Espero
+// BS Cloud and Systems Administration
+// Western Governors University
+
+// #Web-Dev-Bootcamp-Udemy
+
+// main code
+
+// include the packages that we need
+var cats = require("cat-me");
+var jokes = require("knock-knock-jokes");
+
+
+console.log("This message is from app.js");
+console.log(cats());
+console.log(jokes());
